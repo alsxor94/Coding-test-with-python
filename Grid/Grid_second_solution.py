bb74fab9fc7ee7e789b0122b3aa3e6f274c24b5d
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-04 16:26:24 +0900

    Update Grid_second_solution.py

--- Grid/Grid_second_solution.py
+++ Grid/Grid_second_solution.py
@@ -5,11 +5,14 @@ result = 0
 # 한 줄씩 입력 받아 확인하기
 for i in range(n):
     data = list(map(int, input().split()))
-    # 현재 줄에서 '가장 작은 수' 찾기
+    # i행에서 '가장 작은 수' 찾기
     min_value = 10001
     for a in data:
         min_value = min(min_value, a)
-    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+        
+    # 행들을 돌면서 행에서 가장 작은 수중 가장 큰 수 남기기
     result = max(result, min_value)
 
 print(result) # 최종 답안 출력
+#이렇게 하면 몇 번째 행인지 알 수 없고, 더 구체적인 값을 찾을 수 없어서. 좋지 않다고 할 수 있다.
+#하지만 원하는 값만 찾기 위해서는 가장 빠른 방법.
