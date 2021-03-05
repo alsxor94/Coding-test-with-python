2740ad82dee16a1da0c15ca5f15b80fabf744c4c
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-05 14:41:57 +0900

    Update Greedy_6_solution.py

--- Greedy/Greedy_6_solution.py
+++ Greedy/Greedy_6_solution.py
@@ -1,19 +1,20 @@
 import sys
 n = int(sys.stdin.readline())
 list_plus = []
+list_1 = []
 list_minus = []
-while True:
+while n > 0:
     n = n - 1
     a = int(sys.stdin.readline())
-    if a >= 0:
+    if a > 1:
         list_plus.append(a)
+    elif a == 1:
+        list_1.append(a)
     else:
         list_minus.append(a)
-    if n == 0:
-        break
 
 list_plus.sort()
-list_minus.sort()
+list_minus.reverse()
 
 sum_plus = 0
 if len(list_plus) % 2 == 0:
@@ -24,7 +25,6 @@ else:
         sum_plus += list_plus[2 * i + 1] * list_plus[2 * (i + 1)]
     sum_plus = sum_plus + list_plus[0]
 
-
 sum_minus = 0
 if len(list_minus) % 2 == 0:
     for i in range(len(list_minus) // 2):
@@ -32,6 +32,6 @@ if len(list_minus) % 2 == 0:
 else:
     for i in range((len(list_minus) - 1) // 2):
         sum_minus += list_minus[2 * i + 1] * list_minus[2 * (i + 1)]
-    sum_minus = sum_minus + list_minus[0]
+    sum_minus = sum_minus + list_minus[-1]
 
-print(sum_plus + sum_minus)
+print(sum_plus + sum_minus + sum(list_1))
