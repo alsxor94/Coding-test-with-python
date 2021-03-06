c3f8461968d34ca31acf84e6f50553411ce287d9
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-06 16:02:36 +0900

    Update Greedy_6_solution.py
    -1 부분 을 제대로 로직을 못 짰었음

--- Greedy/Greedy_6_solution.py
+++ Greedy/Greedy_6_solution.py
@@ -17,36 +17,38 @@ while n > 0:
         list_minus.append(a)
 
 list_plus.sort()
-list_minus.reverse()
-
+list_plus.reverse()
+list_minus.sort()
 sum_plus = 0
 # calculating by seperating situation odd or even number
-if len(list_plus) % 2 == 0:
-    for i in range(len(list_plus) // 2):
-        sum_plus += list_plus[2 * i] * list_plus[2 * i+1]
-else:
-    for i in range((len(list_plus) - 1) // 2):
-        sum_plus += list_plus[2 * i + 1] * list_plus[2 * (i + 1)]
-    sum_plus = sum_plus + list_plus[0]
+for i in range(len(list_plus) // 2):
+    sum_plus += list_plus[2 * i] * list_plus[2 * i + 1]
+# when the number of positive is odd
+if len(list_plus) % 2 != 0:
+    sum_plus = sum_plus + list_plus[-1]
 
 sum_minus = 0
 # calculating by seperating situation odd or even number
-if len(list_minus) % 2 == 0:
-    for i in range(len(list_minus) // 2):
-        sum_minus += list_minus[2 * i] * list_minus[2 * i+1]
-
-else:
-    for i in range((len(list_minus) - 1) // 2):
-        sum_minus += list_minus[2 * i + 1] * list_minus[2 * (i + 1)]
+for i in range(len(list_minus) // 2):
+    sum_minus = sum_minus + list_minus[2 * i] * list_minus[2 * i + 1]
+# when the number of negative is odd
+if len(list_minus) % 2 != 0:
     if len(list_11) > 0:
         sum_minus = sum_minus + list_minus[-1] * list_11.pop()
-# existence and nonexistence of 0
-if len(list_11) % 2 != 0:
-    if len(list_0) > 0:
-        sum_11 = (len(list_11) - 1) // 2
+    elif len(list_0) > 0:
+        sum_minus = sum_minus
+        list_0.pop()
     else:
-        sum_11 = (len(list_11) - 1) // 2 - 1
-else:
-    sum_11 = len(list_11) // 2
+        sum_minus = sum_minus + list_minus[-1]
+# existence and nonexistence of 0
+sum_11 = 0
+if len(list_11) > 0:
+    if len(list_11) % 2 == 0:
+        sum_11 = len(list_11) // 2
+    else:
+        if len(list_0) > 0:
+            sum_11 = len(list_11) // 2
+        else:
+            sum_11 = len(list_11) // 2 - 1
 
 print(sum_plus + sum_minus + sum(list_1) + sum_11)
