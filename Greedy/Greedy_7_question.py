fae4f0ebd9cc647bda5b39b7edf3120c2707ebee
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-06 20:52:12 +0900

    Update Greedy_7_question.py

--- Greedy/Greedy_7_question.py
+++ Greedy/Greedy_7_question.py
@@ -13,43 +13,37 @@
 # 다음 K개 줄에는 가방에 담을 수 있는 최대 무게 Ci가 주어진다. (1 ≤ Ci ≤ 100,000,000)
 #
 # 모든 숫자는 양의 정수이다.
+from sys import stdin
+n, k = map(int, stdin.readline().split()) # number of jewellery, number of bag
+jew = []; bag = []
 
-import time
-start = time.time()
-
-n, k = map(int, input().split()) # number of jewellery, number of bag
-jew_mass = []
-jew_val = []
-while n > 0:
+for i in range(n):
     n = n - 1
-    m, v = map(int, input().split()) # mass, value of jewellery
-    jew_mass.append(m)
-    jew_val.append(v)
+    m, v = map(int, stdin.readline().split())
+    jew.append([v, m])# mass, value of jewellery
+jew.reverse()
 
-print(jew_mass, jew_val)
-
-bag = []
-for i in range(k):
-    bag.append(int(input())) # volume of bag
+for k in range(k):
+    k = k - 1
+    bag.append(int(stdin.readline())) # volume of bag
 bag.sort()
 
 # 가방마다 무게가 정해져 있고, 어차피 가방 안엔 하나 밖에 못 들어감. 그럼 가치가 젤 높은 걸, 먼저 순서를 찾고, 가방에 들어갈 수 있는 지를 찾고,
 # 가치가 가장 높은 것부터, 무게를 가장 적게 옮길 수 있는 가방에 넣으면 됨
 summa = 0
-
-while len(bag) > 0:
-    max_val_index = jew_val.index(max(jew_val))
-    max_val = jew_val.pop(max_val_index)
-    max_mass = jew_mass.pop(max_val_index)
-    print(max_mass, max_val)
-    for i in bag:
-        if i >= max_mass:
-            summa = summa + max_val
-            bag.remove(i)
-
+for i in jew:
+    for j in bag:
+        if j >= i[1]:
+            summa = summa + i[0]
+            bag.remove(j)
+            break
 print(summa)
-
-end = time.time()
-print(end - start)
-
-#인풋을 한 번에 받는 방법을 몰랐음
+# 4 4
+# 1 100
+# 2 200
+# 13 300
+# 10 500
+# 10
+# 10
+# 10
+# 14
