c3b7560fce335ad9101e02325467995b99916ec4
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-07 11:24:16 +0900

    Update Implementation_1_question.py

--- Implementation/Implementation_1_question.py
+++ Implementation/Implementation_1_question.py
@@ -1,1 +1,25 @@
-d
+#상화좌우
+# 5
+# R R R U D D
+# 3 4
+
+from sys import stdin
+
+n = int(stdin.readline())
+direction = list(stdin.readline().split())
+x = 1; y =1
+for i in direction:
+    if i == 'R':
+        if x + 1 <= n:
+            x += 1
+    elif i == 'L':
+        if x - 1 > 0:
+            x -= 1
+    elif i == 'D':
+        if y + 1 <= n:
+            y += 1
+        print('D', y)
+    elif i == 'U':
+        if y - 1 > 0:
+            y -= 1
+print(y, x)
