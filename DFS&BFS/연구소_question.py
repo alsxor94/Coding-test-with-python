2c5acbecdaefaba8c9d0a2ad9244301fe70c053a
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-24 14:53:48 +0900

    Update 연구소_question.py
    예제

--- DFS&BFS/연구소_question.py
+++ DFS&BFS/연구소_question.py
@@ -3,7 +3,6 @@ from itertools import combinations as combi
 from collections import deque
 import copy
 from time import time
-# brute force algorithm을 알았어야함.
 
 def find_2(graph):
     for i in range(row):
@@ -13,6 +12,8 @@ def find_2(graph):
 
 # spreading virus
 def bfs(r, c, step):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     queue = deque()
     queue.append((r, c))
     while queue:
@@ -29,47 +30,35 @@ def bfs(r, c, step):
 # 다른 사람들은 벽을 채울 수 있는 가짓 수를 다 찾아서 함. combination implementation.
 def fence():
     global result
-    for a, b, c in combi(range(1, row * column), 3):
+    find_2(graph)
+    for a, b, c in combi(range(0, row * column - 1), 3):
         x1, y1 = a % column, a // column
         x2, y2 = b % column, b // column
         x3, y3 = c % column, c // column
-        if graph[y1][x1] == '0' and graph[y2][x2] == '0' and graph[y3][x3] == '0':
-            graph[y1][x1] = '1'
-            graph[y2][x2] = '1'
-            graph[y3][x3] = '1'
-            step = copy.deepcopy(graph)
+
+        step = copy.deepcopy(graph)
+        if step[y1][x1] == '0' and step[y2][x2] == '0' and step[y3][x3] == '0':
+            step[y1][x1] ,step[y2][x2], step[y3][x3] = '1', '1', '1'
             for vy, vx in pos_2:
                 bfs(vy, vx, step)
-            graph[y1][x1] = '0'
-            graph[y2][x2] = '0'
-            graph[y3][x3] = '0'
-            # for i in step:
-            #     print(i)
             tmp = 0
             for i in step:
                 tmp += i.count('0')
             result = max(tmp, result)
-            # print(x1, y1, x2, y2, x3, y3, result)
         else:
             continue
 
 
 
-st = time()
 row, column = map(int, stdin.readline().split())
+st = time()
 graph = []
 result = 0
-result_1 = []
 pos_2 = []
 for i in range(row):
     graph.append(stdin.readline().split())
 
-cnt = 0
-find_2(graph)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 fence()
 print(result)
 en = time()
-print(en-st) # 3.2s
+print(en-st) # 2.56s 예제3
