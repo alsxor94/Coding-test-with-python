2bf1a354dc908a61824fc9874c36bcc112ed7fda
alsxor94 <43167454+alsxor94@users.noreply.github.com>
2021-03-24 10:53:41 +0900

    Update 연구소_question.py

--- DFS&BFS/연구소_question.py
+++ DFS&BFS/연구소_question.py
@@ -1,17 +1,20 @@
 from sys import stdin
+from itertools import combinations as combi
 from collections import deque
-row, column = map(int, stdin.readline().split())
-
+import copy
+from time import time
 # bfs로 해야할듯? 길이 전부 파악하고, 그 뒤 2 위치 찾아서 거리 가장 가까운 곳에 벽을 세우면 될 듯.
 # --> 논리가 틀림.
 
+# is it useful?
 def find_2(graph):
     for i in range(row):
         for j in range(column):
             if '2' == graph[i][j]:
                 pos_2.append([i, j])
 
-def bfs(r, c):
+# searching safe area
+def bfs(r, c, step):
     queue = deque()
     queue.append((r, c))
     while queue:
@@ -19,41 +22,58 @@ def bfs(r, c):
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if ny < 0 or ny >= row or nx < 0 or nx >= column or graph[ny][nx] == '1' or graph[ny][nx] == '2':
+            if ny < 0 or ny >= row or nx < 0 or nx >= column or step[ny][nx] == '1':
                 continue
-            if graph[ny][nx] == '0' and visited[ny][nx] == 0:
-                visited[ny][nx] = visited[y][x] + 1
+            if step[ny][nx] == '0':
+                step[ny][nx] = '2'
                 queue.append((ny, nx))
 
 # 영역의 개념을 어떻게 구현할 수 있을까? 싸였다라는 개념을 어케 넣지?
 # 어떻게 벽을 세울까? 무슨 논리로?
+# 다른 사람들은 벽을 채울 수 있는 가짓 수를 다 찾아서 함. combination implementation.
 def fence():
-    return 1
+    global result
+    for a, b, c in combi(range(1, row * column), 3):
+        x1, y1 = a % column, a // column
+        x2, y2 = b % column, b // column
+        x3, y3 = c % column, c // column
+        if graph[y1][x1] == '0' and graph[y2][x2] == '0' and graph[y3][x3] == '0':
+            graph[y1][x1] = '1'
+            graph[y2][x2] = '1'
+            graph[y3][x3] = '1'
+            step = copy.deepcopy(graph)
+            for vy, vx in pos_2:
+                bfs(vy, vx, step)
+            graph[y1][x1] = '0'
+            graph[y2][x2] = '0'
+            graph[y3][x3] = '0'
+            # for i in step:
+            #     print(i)
+            tmp = 0
+            for i in step:
+                tmp += i.count('0')
+            result = max(tmp, result)
+            # print(x1, y1, x2, y2, x3, y3, result)
+        else:
+            continue
 
 
 
-
+st = time()
+row, column = map(int, stdin.readline().split())
 graph = []
+result = 0
+result_1 = []
 pos_2 = []
-step = []
 for i in range(row):
     graph.append(stdin.readline().split())
-    step.append([0]*column)
 
+cnt = 0
 find_2(graph)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-for r, c in pos_2:
-    print(r, c)
-    visited = []
-    for i in range(row):
-        visited.append([0] * column)
-    bfs(r, c)
-    for k in range(row):
-        print(visited[k])
-        for j in range(column):
-            step[k][j] += visited[k][j]
 
-print()
-for i in step:
-    print(i)
+fence()
+print(result)
+en = time()
+print(en-st) # 3.2s
